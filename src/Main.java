7381b5465f4982fb86c9486fba73cf25b3e00594
MarcoCipolletta <marcocipolletta1998@gmail.com>
2024-11-18 16:06:23 +0100

    Esercizio 2

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,30 @@ public class Main {
 
         //Es1
         Es1 esercizio1 = new Es1();
-        String[] arrayIniziale = {"a","b","c","d","e"};
+        String[] arrayIniziale = {"a", "b", "c", "d", "e"};
         String stringaDaAggiungere = "Nuova";
-        String[] nuovoArr = esercizio1.inserisciInArray(arrayIniziale,stringaDaAggiungere);
+        String[] nuovoArr = esercizio1.inserisciInArray(arrayIniziale, stringaDaAggiungere);
 
         for (int i = 0; i < nuovoArr.length; i++) {
 
-        System.out.println(nuovoArr[i]);
+            System.out.println(nuovoArr[i]);
         }
+
+        //Es2
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Inserisci la prima stringa");
+        String a = scanner.nextLine();
+        System.out.println("Inserisci la seconda stringa");
+        String b = scanner.nextLine();
+        System.out.println("Inserisci la terza stringa");
+        String c = scanner.nextLine();
+
+        System.out.println("in ordine: " + a + " " + b + " " + c);
+        System.out.println("al contrario: " + c + " " + b + " " + a);
+
+
+
 
     }
 
